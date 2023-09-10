807d933d59026c8dffdc6caf49f6bbe0d17dc43f
Rene Garcia <rene.garcia46@uabc.edu.mx>
2023-09-09 20:40:40 -0700

    Meta 1.3

--- Programa_Amazon_1p3.py
+++ Programa_Amazon_1p3.py
@@ -1,6 +1,7 @@
-#Selenium
-
-
+"""Garcia Soto Rene
+Fecha: 09/09/23
+Realizar una función en python que reciba el nombre de un producto en Amazon y automatice la busqueda del producto.
+ El script debe tomar una impresión de pantalla despues de realizar la busqueda."""
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -12,20 +13,14 @@ s= Service(ChromeDriverManager().install())
 opc=Options()
 opc.add_argument("--windows-size=1020,1200")
 
-
-
 navegador=webdriver.Chrome(service=s,options=opc)
 navegador.get("https://www.amazon.com.mx/")
 txt=navegador.find_element(By.ID,"twotabsearchtextbox")
 producto=input("Que producto vas a buscar---> ")
 txt.send_keys(producto)
 time.sleep(2)
-"""password=navegador.find_element(By.NAME,"pass")
-password.send_keys("Tainy")
-time.sleep(2)"""
-
-sesion=navegador.find_element(By.ID,"nav-search-submit-button")
-sesion.click()
+buscar=navegador.find_element(By.ID,"nav-search-submit-button")
+buscar.click()
 navegador.find_elements()
 navegador.save_screenshot("Producto.png")
 print(navegador.title)
